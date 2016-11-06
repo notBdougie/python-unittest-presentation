da8e0596d80c51311eb38b424b660410ebd99f4f
Markus Opolka <markus@martialblog.de>
2016-11-06 15:15:39 +0100

    Update exercise

--- code/exercise/battleship.py
+++ code/exercise/battleship.py
@@ -3,6 +3,55 @@
 
 from random import randint
 from sys import exit
+from collections import namedtuple
+import functools
+
+
+def color_it(color=None):
+
+    ENDC = '\033[0m'
+
+    def decorator(func):
+        @functools.wraps(func)
+
+        def wrapper(*args, **kwargs):
+            print(color)
+            func(*args, **kwargs)
+            print(ENDC)
+        return wrapper
+
+    return decorator
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+
+
+def print_turn(current, maximum):
+    print ("Turn {0} of {1}".format(current, maximum))
+
+def print_intro():
+    print("\n === Let's play Battleship! === \n")
+
+@color_it(color=Colors.WARNING)
+def print_oob():
+    print("Whoa, that's not even in the ocean!")
+
+@color_it(color=Colors.OKBLUE)
+def print_guess():
+    print("You guessed that one already.")
+
+@color_it(color=Colors.OKGREEN)
+def print_hit():
+    print("Congratulations! You sunk my battleship!")
+
+@color_it(color=Colors.WARNING)
+def print_miss():
+    print("You missed my battleship!")
 
 
 class Board:
@@ -53,39 +102,35 @@ class Guess:
         self.col = col
 
 
-def print_intro():
-
-    intro = "\n === Let's play Battleship! === \n"
-    print(intro)
-
-
-def print_turn(current, maximum):
-
-    print ("Turn {0} of {1}".format(current, maximum))
-
-
-def check_for_hit(guess, ship, board):
+def is_out_of_boundary(guess, board):
 
     max_size = board.size - 1
     out_of_boundary = (guess.row < 0 or guess.row > max_size) or (guess.col < 0 or guess.col > max_size)
 
     if out_of_boundary:
-        print("Whoa, that's not even in the ocean!\n")
-        return
+        print_oob()
+        return True
+    else:
+        return False
 
+
+def check_for_hit(guess, ship, board):
+
+    if is_out_of_boundary(guess, board):
+        return
 
     guessed_already = (board.board[guess.row][guess.col] == "X")
     sunk_battleship = guess.row == ship.row and guess.col == ship.col
 
     if sunk_battleship:
-        print("Congratulations! You sunk my battleship!")
+        print_hit()
         exit(0)
 
     elif guessed_already:
-        print("You guessed that one already.")
+        print_guess()
 
     else:
-        print("You missed my battleship!")
+        print_miss()
         board.board[guess.row][guess.col] = "X"
 
 
@@ -96,30 +141,28 @@ def main(debug=False):
 
     board = Board(board_size)
 
-    ship_row = board.random_row()
-    ship_col = board.random_col()
-
-    ship = Ship(ship_row, ship_col)
+    ship = Ship(
+        board.random_row(),
+        board.random_col())
 
     if debug:
         print('Where the ship at:')
         print('Row: {0} Col: {1}'.format(ship.row, ship.col))
 
     print_intro()
-    board.print_board()
 
     for turn in range(max_turns):
 
         print_turn(str(turn+1), str(max_turns))
 
-        guess_row = int(input("Guess Row:"))
-        guess_col = int(input("Guess Col:"))
+        board.print_board()
+
+        guess_row = int(input("Guess Row: "))
+        guess_col = int(input("Guess Col: "))
         guess = Guess(guess_row, guess_col)
 
         check_for_hit(guess, ship, board)
 
-        board.print_board()
-
 
 if __name__ == "__main__":
-    main(True)
+    main()
